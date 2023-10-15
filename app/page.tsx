08c4e04ae952e9cabe17625741c9e25ae03ed425
ms-yuuta <massu.code@gmail.com>
2023-10-15 11:20:50 +0900

    💄

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-7xl w-full justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-8xl w-full justify-between font-mono text-sm lg:flex">
         <p className="fixed text-xl left-16 top-0 flex w-full justify-center border-b font-mincho font-bold pb-6 pt-8 backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 lg:bg-zinc-800/30">
           {title}
         </p>
